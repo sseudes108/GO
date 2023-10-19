1c7df363fe538d1051635e23bbac3d9099414d2d
Senstur Games <senstur.art@gmail.com>
2023-10-19 19:06:03 -0300

    0.7

--- lectures/exercise/sr-library/library.go
+++ lectures/exercise/sr-library/library.go
@@ -24,135 +24,136 @@ import (
 	"time"
 )
 
-/*
-Struct livros
-//Struct members
-funçao de emprestimo (livro + membro) (horario de emprestimo)
-função de entrega (livro + membro) (horario de emprestimo)
-função de verificar o status de todos os livros
-*/
+type Title string
+type Name string
 
-// Membros
-const (
-	membroPadrao = 0
-	alessandra   = 1
-	bianca       = 2
-	danny        = 3
-)
-
-// Livros
-const (
-	guia1   = 0
-	ecce    = 1
-	fredo   = 2
-	candido = 3
-)
-
-type Book struct {
-	index             int
-	nome              string
-	emprestado        bool
-	emprestadoPorQuem Member
-	emprestadoQuando  time.Time
+type LendAudit struct {
+	checkOut time.Time
+	checkIn  time.Time
 }
 
 type Member struct {
-	nome        string
-	quantLivros int
-	livros      []Book
+	name  Name
+	books map[Title]LendAudit
 }
 
-func checarLib(books []*Book, clients []*Member) {
-	fmt.Println("Status da lib")
-	fmt.Println("---")
-	for _, book := range books {
-		if !book.emprestado {
-			fmt.Println("Nome:", book.nome, "/ Emprestado? Não")
+type BookEntry struct {
+	total  int
+	lended int
+}
+
+type library struct {
+	members map[Name]Member
+	books   map[Title]BookEntry
+}
+
+func printMemberAudit(member *Member) {
+	for title, audit := range member.books {
+		var returnTime string
+		if audit.checkIn.IsZero() {
+			returnTime = "[Not returned yet]"
 		} else {
-			fmt.Println("Nome:", book.nome, "/ Emprestado? Sim")
+			returnTime = audit.checkIn.String()
 		}
+		fmt.Println(member.name, ":", title, ":", audit.checkOut.String(), "through", returnTime)
+		//fmt.Println("1")
 	}
-
-	fmt.Println("---")
-	//fmt.Println("Membros com livros emprestados")
-	for _, client := range clients {
-		if client.quantLivros > 0 {
-			//fmt.Println(client.nome, "/ Livros emprestados", client.quantLivros)
-			checarClients(client)
-		}
-	}
-	//fmt.Println("---")
 }
 
-func checarClients(client *Member) {
-	fmt.Println("Livros em posse de", client.nome, ":")
-	for _, book := range client.livros {
-		fmt.Println(book.nome, ", desde", book.emprestadoQuando.Format("2006-01-02 15:04"))
+func printMembersAudit(library *library) {
+	for _, member := range library.members {
+		printMemberAudit(&member)
+		//fmt.Println("0")
 	}
-	fmt.Println("---")
 }
 
-func emprestarLivro(book *Book, client *Member) {
-	fmt.Println("Emprestando livro...")
-	book.emprestado = true
-	book.emprestadoPorQuem = *client
-	book.emprestadoQuando = time.Now()
-	formattedTime := book.emprestadoQuando.Format("2006-01-02 15:04")
-	client.quantLivros++
-	client.livros = append(client.livros, *book)
-	fmt.Println("Livro:", book.nome, "Emprestado para", client.nome, "em:", formattedTime)
-
-	fmt.Println("---")
+func printLibraryBooks(library *library) {
+	fmt.Println()
+	for title, book := range library.books {
+		fmt.Println(title, "/ total:", book.total, "/ Lended", book.lended)
+	}
 }
 
-func devolverLivro(book *Book, client *Member, membros []*Member) {
-	fmt.Println("Devolvendo livro...")
-	fmt.Println("Livro:", book.nome, "Devolvido por", client.nome, "em:", time.Now().Format("2006-01-02 15:04"))
+func checkOutBook(library *library, title Title, member *Member) bool {
+	book, found := library.books[title]
+	if !found {
+		fmt.Println("Book no part of library")
+		return false
+	}
+	if book.lended == book.total {
+		fmt.Println("No more books to lend")
+		return false
+	}
 
-	book.emprestado = false
-	client.quantLivros--
-	client.quantLivros[book.index]
-	client = membros[membroPadrao]
-	book.emprestadoPorQuem = *client
+	book.lended++
+	library.books[title] = book
+	member.books[title] = LendAudit{checkOut: time.Now()}
 
-	fmt.Println("---")
+	fmt.Println("Checked out", title, "to", member.name)
+	return true
+}
+
+func returnBook(library *library, title Title, member *Member) bool {
+	book, found := library.books[title]
+	if !found {
+		fmt.Println("Book no part of library")
+		return false
+	}
+	audit, found := member.books[title]
+	if !found {
+		fmt.Println("Member did not check out this book")
+		return false
+	}
+
+	book.lended--
+	library.books[title] = book
+
+	audit.checkIn = time.Now()
+	member.books[title] = audit
+
+	return true
 }
 
 func main() {
 
-	//Livros
-
-	books := []*Book{
-		{nome: "O guia do mochileiro das galaxias", index: guia1},
-		{nome: "Ecce Homo", index: ecce},
-		{nome: "Fredo", index: fredo},
-		{nome: "Cândido", index: candido},
+	library := library{
+		books:   make(map[Title]BookEntry),
+		members: make(map[Name]Member),
 	}
 
-	membros := []*Member{
-		{nome: "membroPadrao"},
-		{nome: "Alessandra Ribeiro"},
-		{nome: "Bianca Hills"},
-		{nome: "Danny Bendochy"},
+	library.books["O guia do Mochileiro das Galaxias"] = BookEntry{
+		total:  2,
+		lended: 0,
+	}
+	library.books["Ecce Homo"] = BookEntry{
+		total:  1,
+		lended: 0,
+	}
+	library.books["Candido"] = BookEntry{
+		total:  1,
+		lended: 0,
+	}
+	library.books["Fredo"] = BookEntry{
+		total:  1,
+		lended: 0,
 	}
 
-	checarLib(books, membros)
+	library.members["Alessandra"] = Member{"Alessandra Ribeiro", make(map[Title]LendAudit)}
+	library.members["Danny"] = Member{"Danny Bendochy", make(map[Title]LendAudit)}
+	library.members["Juliana Souza"] = Member{"Juliana Souza", make(map[Title]LendAudit)}
 
-	emprestarLivro(books[guia1], membros[alessandra])
+	fmt.Println("Initials")
+	printLibraryBooks(&library)
+	printMembersAudit(&library)
 
-	checarLib(books, membros)
+	member := library.members["Alessandra"]
+	fmt.Println("Checkout a book")
+	checkOutBook := checkOutBook(&library, "Ecce Homo", &member)
+	if checkOutBook {
+		printLibraryBooks(&library)
+		printMembersAudit(&library)
+	} else {
+		fmt.Println("Error")
+	}
 
-	devolverLivro(books[guia1], membros[alessandra], membros)
-
-	checarLib(books, membros)
-
-	emprestarLivro(books[ecce], membros[danny])
-	emprestarLivro(books[fredo], membros[danny])
-	emprestarLivro(books[candido], membros[bianca])
-
-	checarLib(books, membros)
-
-	devolverLivro(books[ecce], membros[danny], membros)
-
-	checarLib(books, membros)
 }
