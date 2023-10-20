b05d3c31aa24c236d2e712b90ba81f395cb7f399
Senstur Games <senstur.art@gmail.com>
2023-10-20 19:38:05 -0300

    1.0

--- lectures/demo/variadics/variadics.go
+++ lectures/demo/variadics/variadics.go
@@ -2,6 +2,38 @@ package main
 
 import "fmt"
 
-func main() {
+func sum(nums ...int) int {
+	sum := 0
+	for _, number := range nums {
+		sum += number
+	}
+	return sum
+}
 
+/*func fib() {
+
+	a := 1 // 1
+	b := 1 // 1
+	c := 0
+
+	fmt.Println(1)
+	fmt.Println(1)
+
+	for i := 0; i < 10; i++ {
+		c = a + b
+		//------------//
+		a = b
+		b = c
+		fmt.Println(c)
+	}
+}*/
+
+func main() {
+	a := []int{1, 2, 3, 1, 2, 3, 1, 2, 3} //18
+	b := []int{1, 2, 3, 1, 2, 3, 1, 2, 3} //18
+
+	all := append(a, b...)
+
+	answer := sum(all...)
+	fmt.Println(answer)
 }
--- lectures/exercise/iota/iota.go
+++ lectures/exercise/iota/iota.go
@@ -16,7 +16,40 @@ package main
 
 import "fmt"
 
+// * Mathematical operations must be defined as constants using iota
+type Operation int
+
+const (
+	add Operation = iota
+	sub
+	mul
+	div
+	shiv
+)
+
+// Receiver function that performs the mathematical operation on two operands
+// * Operations required:
+//   - Add, Subtract, Multiply, Divide
+func (Operator Operation) calculate(number1 int, number2 int) int {
+	switch Operator {
+	case add:
+		return number1 + number2
+	case sub:
+		return number1 - number2
+	case mul:
+		return number1 * number2
+	case div:
+		return number1 / number2
+	default:
+		return 0000
+	}
+
+}
+
 func main() {
+
+	//add,sub,mul and div created as consts
+
 	fmt.Println(add.calculate(2, 2)) // = 4
 
 	fmt.Println(sub.calculate(10, 3)) // = 7
