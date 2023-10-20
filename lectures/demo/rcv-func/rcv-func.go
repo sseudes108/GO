3f91298d26c1ecd5cd345e516eba18388760049d
Senstur Games <senstur.art@gmail.com>
2023-10-20 07:46:13 -0300

    0.9

--- lectures/demo/rcv-func/rcv-func.go
+++ lectures/demo/rcv-func/rcv-func.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
+type Space struct {
+	occupied bool
+}
+type ParkinLot struct {
+	spaces []Space
+}
 
+func occupySpace(lot *ParkinLot, spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *ParkinLot) occupySpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *ParkinLot) vacateSpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = false
+}
+
+func main() {
+	lot := ParkinLot{spaces: make([]Space, 5)}
+	fmt.Println(lot)
+
+	lot.occupySpace(1)
+	occupySpace(&lot, 3)
+
+	fmt.Println(lot)
+	lot.vacateSpace(3)
+
+	fmt.Println(lot)
 }
