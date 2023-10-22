e1f48595f426feaea3e194a0cb7c02edac0c960d
Senstur Games <senstur.art@gmail.com>
2023-10-22 20:41:06 -0300

    1.4

--- lectures/demo/errors/errors.go
+++ lectures/demo/errors/errors.go
@@ -4,6 +4,25 @@ import (
 	"fmt"
 )
 
+type Stuff struct {
+	values []int
+}
+
+func (s *Stuff) Get(index int) (int, error) {
+	if index > len(s.values) {
+		return 0, fmt.Errorf("no elements at index  %v", index)
+	} else {
+		return s.values[index], nil
+	}
+}
+
 func main() {
 
+	stuff := Stuff{}
+	value, err := stuff.Get(10)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("value is", value)
+	}
 }
--- lectures/exercise/errors/errors.go
+++ lectures/exercise/errors/errors.go
@@ -15,3 +15,59 @@
 //* Use the `errors` package to generate errors
 
 package timeparse
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Time struct {
+	hour, minute, second int
+}
+
+type TimeParseError struct {
+	msg   string
+	input string
+}
+
+func (t *TimeParseError) Error() string {
+	return fmt.Sprintf("%v: %v", t.msg, t.input)
+}
+
+// Example time string: 14:07:33
+func ParseTime(input string) (Time, error) {
+	//SPLIT THE STRING 14:07:33 PELO SEPARADOR DETERMINADO NA FUNÇÃO SPLIT ( : NO CASO)
+	components := strings.Split(input, ":")
+
+	if len(components) != 3 {
+		return Time{}, &TimeParseError{"Invalid number of time components", input}
+	} else { //ATOI CONVERTE CADA PARTE DA STRING SEPARADA ANTERIORMENTE PARA O VALOR EM INT
+		hour, err := strconv.Atoi(components[0])
+		if err != nil {
+			return Time{}, &TimeParseError{fmt.Sprintf("Err parsing hour: %v", err), input}
+		}
+		minutes, err := strconv.Atoi(components[1])
+		if err != nil {
+			return Time{}, &TimeParseError{fmt.Sprintf("Err parsing minutes: %v", err), input}
+		}
+		seconds, err := strconv.Atoi(components[2])
+		if err != nil {
+			return Time{}, &TimeParseError{fmt.Sprintf("Err parsing seconds: %v", err), input}
+		}
+
+		if hour > 23 || hour < 0 {
+			return Time{}, &TimeParseError{"hour out of  range: 0 <= hour <= 23", fmt.Sprintf("%v", hour)}
+		}
+
+		if minutes > 59 || minutes < 0 {
+			return Time{}, &TimeParseError{"minute out of  range: 0 <= minute <= 59", fmt.Sprintf("%v", minutes)}
+		}
+
+		if seconds > 59 || seconds < 0 {
+			return Time{}, &TimeParseError{"seconds out of  range: 0 <= seconds <= 59", fmt.Sprintf("%v", seconds)}
+		}
+
+		return Time{hour, minutes, seconds}, nil
+	}
+}
