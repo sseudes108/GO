6e01050db3cb932995b405f0c4b61303a8cb1b56
Senstur Games <senstur.art@gmail.com>
2023-10-28 09:25:15 -0300

    2.2

--- lectures/demo/readers/readers.go
+++ lectures/demo/readers/readers.go
@@ -1,8 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	r := bufio.NewReader(os.Stdin)
+	sum := 0
+
+	for {
+		input, inputErr := r.ReadString(' ')
+		n := strings.TrimSpace(input)
+		if n == "" {
+			continue
+		}
+		num, convError := strconv.Atoi(n)
+		if convError != nil {
+			fmt.Println(convError)
+		} else {
+			sum += num
+		}
+
+		if inputErr == io.EOF {
+			break
+		}
+		if inputErr != nil {
+			fmt.Println("Error reading Stdin:", inputErr)
+		}
+	}
+	fmt.Printf("sum: %v\n", sum)
 }
--- lectures/exercise/reader/reader.go
+++ lectures/exercise/reader/reader.go
@@ -18,8 +18,54 @@
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+//   - Upon program exit, some usage statistics should be printed
+//     ('Q' and 'q' do not count towards these statistics):
+//   - The number of non-blank lines entered
+//   - The number of commands entered
+func ExitProgram(comandsEntered int, nonBlankLines int) {
+	fmt.Println("Comands entered:", comandsEntered)
+	fmt.Println("Non-blank lines entered:", nonBlankLines)
+	os.Exit(0)
+}
 
 func main() {
+	userInput := bufio.NewReader(os.Stdin)
+	comandsEntered := 0
+	nonBlankLines := 0
+
+	for {
+		input, err := userInput.ReadString('\n')
+		inpText := strings.TrimSpace(input)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		//* When the user enters either "hello" or "bye", the program
+		//  should respond with a message after pressing the enter key.
+		if inpText != "" {
+			if inpText == "Hello" || inpText == "hello" || inpText == "hi" || inpText == "Hi" {
+				fmt.Println("Hello, Dave")
+				comandsEntered++
+			} else if inpText == "Bye" || inpText == "bye" {
+				fmt.Println("Goodbye, Dave")
+				comandsEntered++
+			} else if inpText == "Q" || inpText == "q" {
+				ExitProgram(comandsEntered, nonBlankLines)
+			} else {
+				fmt.Printf("Dave, I did not understand that '%v'", inpText)
+				fmt.Println()
+				nonBlankLines++
+			}
+		} else {
+			fmt.Println("no input, Dave")
+		}
+	}
 
 }
