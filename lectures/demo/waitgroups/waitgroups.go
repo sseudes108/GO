5ae5f2edea658531b8afe976e8d521913ce4c10d
Senstur Games <senstur.art@gmail.com>
2023-11-06 07:25:31 -0300

    2.5

--- lectures/demo/waitgroups/waitgroups.go
+++ lectures/demo/waitgroups/waitgroups.go
@@ -1,11 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "sync"
-    "time"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 func main() {
+	var wg sync.WaitGroup
+	counter := 0
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		counter++
+		go func() {
+			defer func() {
+				fmt.Println(counter, "Goroutines remaining")
+				counter--
+				wg.Done()
+			}()
+			duration := time.Duration(rand.Intn(500) * int(time.Millisecond))
+			fmt.Println("Waiting for", duration)
+			time.Sleep(duration)
+		}()
+	}
+
+	fmt.Println("Waiting for goroutines to finish")
+	wg.Wait()
+	fmt.Println("Done!")
 }
