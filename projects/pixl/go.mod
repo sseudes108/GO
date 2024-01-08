5e3a365f1bff0e5256836dd5e0950e5149abab4f
Basilísco <senstur.art@gmail.com>
2024-01-08 12:02:44 -0300

    4.2

---
+++ projects/pixl/go.mod
@@ -0,0 +1,8 @@
+module zerotomastery.io/pixel
+
+go 1.21.5
+
+require (
+	fyne.io/fyne v1.4.3
+	fyne.io/fyne/v2 v2.4.3
+)
---
+++ projects/pixl/swatch/swatch.go
@@ -0,0 +1,43 @@
+package swatch
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/widget"
+	"zerotomastery.io/pixel/apptype"
+)
+
+type Swatch struct {
+	widget.BaseWidget
+	Selected     bool
+	Color        color.Color
+	SwatchIndex  int
+	ClickHandler func(swatch *Swatch)
+}
+
+func (swatch *Swatch) SetColor(newColor color.Color) {
+	swatch.Color = newColor
+}
+
+func NewSwatch(state *apptype.State, color color.Color, swatchIndex int, clickHandler func(swatch *Swatch)) *Swatch {
+	swatch := &Swatch{
+		Selected:     false,
+		Color:        color,
+		ClickHandler: clickHandler,
+		SwatchIndex:  swatchIndex,
+	}
+	swatch.ExtendBaseWidget(swatch)
+	return swatch
+}
+
+func (swatch *Swatch) CreateRenderer() fyne.WidgetRenderer {
+	square := canvas.NewRectangle(swatch.Color)
+	objects := []fyne.CanvasObject{square}
+	return &SwatchRenderer{
+		square:  *square,
+		objects: objects,
+		parent:  swatch,
+	}
+}
